b33abee413d4848db3479eea72116ebe5e02822d
Juan Camilo González <info@juancgonzalez.com>
2018-12-18 11:28:22 -0500

    init defult tempate

---
+++ default.php
@@ -0,0 +1,7 @@
+<?php defined('AUTOMAD') or die('Direct access not permitted!'); ?>
+
+<@ parts/head.php @>
+  <div class="blockCenter m-90 t-80 d-70 ld-60">
+    <p>@{ text | markdown }</p>
+  </div>
+<@ parts/footer.php @>
