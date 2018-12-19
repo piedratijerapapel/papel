3de73ffae02c63fa031f4998bd189434f84d2d30
Juan Camilo González <info@juancgonzalez.com>
2018-12-19 10:26:52 -0500

    update menu snippet

--- papel.php
+++ papel.php
@@ -3,11 +3,11 @@
 <@ parts/head.php @>
   <div id="homeWrapper" class="m-80 t-80 d-70 ld-60">
     <div id="homeLogo"></div>
-    <div class="viewOptions">
-      <a class="homeTerm" href="/filtros?filter=imgs">imagen.</a>
-      <a class="homeTerm" href="/filtros">palabra.</a>
-      <a class="homeTerm" href="/filtros?filter=abc">índice.</a>
-    </div>
+    <nav class="viewOptions">
+      <@ with '/filtros' @>
+        <@ tree @>
+      <@ end @>
+    </nav>
     <h2 class="homeSubtitle">@{ textTeaser | markdown }</h2>
     <img class="homeContentLogo" src="/shared/logo-silu.png" alt="La silueta">
   </div>
--- parts/nav.php
+++ parts/nav.php
@@ -1,19 +1,17 @@
 <?php defined('AUTOMAD') or die('Direct access not permitted!'); ?>
 <nav class="navContent m-100 t-50 d-40 ld-20">
   <@ snippet tree @>
-    <@ if @{ :currentPath } @>
-      <@ if @{ :pagelistCount } @>
-        <ul class="subNav">
-          <@ foreach in pagelist @>
-            <@ if not @{ checkboxHideInMenu } @>
-            <li<@ if @{ :current } @> class="active"<@ end @>>
-              <a href="@{ url }">@{ title | stripTags }</a>
-              <@ tree @>
-            </li>
-            <@ end @>
-          <@ end @>
-        </ul>
+    <@ if @{ :pagelistCount } @>
+    <ul class="subNav">
+      <@ foreach in pagelist @>
+        <@ if not @{ checkboxHideInMenu } @>
+        <li class="@{:template}<@ if @{:current} @> active<@ end @>">
+          <a href="@{url}">@{title}</a>
+          <@ tree @>
+        </li>
+        <@ end @>
       <@ end @>
+    </ul>
     <@ end @>
   <@ end @>
 
@@ -22,9 +20,8 @@
     excludeHidden: true 
   } @>
   <div class="mainNav">
-    <# Change context to the homepage #>
     <@ with "/" @>
-    <# Call recursive tree snippet #>
-    <@ tree @><@ end @>
+      <@ tree @>
+    <@ end @>
   </div>
 </nav>
