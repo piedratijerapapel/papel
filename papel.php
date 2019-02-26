052e02ee0ca16d3646d3925d5ef8dbd727f38506
Juan Camilo González <info@juancgonzalez.com>
2019-02-26 09:55:35 -0500

    update logos

--- papel.php
+++ papel.php
@@ -1,14 +1,17 @@
 <?php defined('AUTOMAD') or die('Direct access not permitted!'); ?>
 
 <@ parts/head.php @>
-  <div id="homeWrapper" class="m-80 t-80 d-70 ld-60">
-    <div id="homeLogo"></div>
-    <nav class="viewOptions">
-      <@ with '/filtros' @>
-        <@ tree @>
-      <@ end @>
-    </nav>
-    <h2 class="homeSubtitle">@{ textTeaser | markdown }</h2>
-    <img class="homeContentLogo" src="/shared/logo-silu.png" alt="La silueta">
-  </div>
-<@ parts/footer.php @>
+    <div id="homeWrapper" class="contentWrapper m-90 t-80 d-70 ld-60">
+        <div id="homeLogo"></div>
+        <nav class="viewOptions">
+            <@ with '/filtros' @>
+                <@ tree @>
+                    <@ end @>
+        </nav>
+        <h2 class="homeSubtitle">@{ textTeaser | markdown }</h2>
+        <div class="logosWrapper">
+            <img class="homeContentLogo" src="/shared/logos pata.jpg" alt="Logos">
+        </div>
+
+    </div>
+    <@ parts/footer.php @> 
