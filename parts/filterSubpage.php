9f4b0b1ddf26146b59131c67c16b61123784055d
Juan Camilo González <info@juancgonzalez.com>
2019-02-26 09:59:21 -0500

    conditionally set grid for filter pages

--- parts/filterSubpage.php
+++ parts/filterSubpage.php
@@ -1,10 +1,17 @@
 <?php defined('AUTOMAD') or die('Direct access not permitted!'); ?>
 <@ head.php @>
-  <nav id="filtersNav">
-    <@ with @{:parent} @>
-      <@ tree @>
-    <@ end @>
-  </nav>
-  
-  <div id="filtersContent"></div>
-<@ footer.php @>
+    <nav id="filtersNav">
+        <@ with @{:parent} @>
+            <@ tree @>
+                <@ end @>
+    </nav>
+
+    <@ if @{:template}='filterByImage' @>
+        <div id="filtersContent" class="contentWrapper m-100 t-100 d-90 ld-90"></div>
+        <@ else @>
+            <div id="filtersContent" class="contentWrapper m-90 t-80 d-80 ld-80"></div>
+            <@ end @>
+
+
+
+                <@ footer.php @> 
