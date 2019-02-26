d2127fd356211a648572f507d1a547e7991483bd
Juan Camilo González <info@juancgonzalez.com>
2019-02-26 09:55:45 -0500

    remove welcome msg

--- typo.php
+++ typo.php
@@ -1,15 +1,16 @@
 <?php defined('AUTOMAD') or die('Direct access not permitted!'); ?>
 
 <@ parts/head.php @>
-<input id="cursorInput" type="text" />
-<div id="stageWrapper">
-  <canvas id="stage"></canvas>
+    <div class="contentWrapper m-90 t-90 d-90 ld-90">
+        <input id="cursorInput" type="text" />
+        <div id="stageWrapper">
+            <canvas id="stage"></canvas>
 
-  <div id="welcome">
-	  PUEDES ESCRIBIR!
-  </div>
+            <div id="welcome">
+                <!-- PUEDES ESCRIBIR! -->
+            </div>
 
-  <span id="blinkingCursor">|</span>
-</div>
-
-<@ parts/footer.php @>
+            <span id="blinkingCursor">|</span>
+        </div>
+    </div>
+    <@ parts/footer.php @> 
