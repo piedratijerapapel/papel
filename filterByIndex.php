fd64748b90a253e4a693fc26da8b4989ebe2c6db
Juan Camilo González <info@juancgonzalez.com>
2019-03-03 22:46:44 -0500

    add category template and add comments in all filter templates

--- filterByIndex.php
+++ filterByIndex.php
@@ -1,3 +1,6 @@
 <?php defined('AUTOMAD') or die('Direct access not permitted!'); ?>
-
+<?php
+  // same template for all filters, 
+  // separate files for conditionally set template name
+?>
 <@ parts/filterSubpage.php @>
